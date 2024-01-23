66fab419c3ac63fed814929b442172facbc70c18
Caleb <cjmeepers@gmail.com>
2024-01-23 18:16:12 +0800

    test window

---
+++ src/Window/Window.cpp
@@ -0,0 +1,8 @@
+#include "Window.hpp"
+
+#include <iostream>
+
+void Test()
+{
+	std::cout << "Test Complete!" << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include "Window/Window.hpp"
+
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
+
+	Test();
 
 	return 0;
 }
